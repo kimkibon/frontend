38dbbab669cfd2b3fa264cc7148bbf529c77054a
sa <darkcat90@naver.com>
2022-07-25 14:10:46 +0900

    mypageindex수정-sa

--- front/src/myPage/MyPageIndex.js
+++ front/src/myPage/MyPageIndex.js
@@ -5,7 +5,7 @@ import Sidebar from '../commons/Sidebar';
 import MyPage from './MyPage';
 import QnaList from './qna/QnaList';
 import ReportList from './report/ReportList';
-import ReserveList from './reserve/ReserveList';
+import ReserveListPage from './reserve/ReserveListPage';
 import ReviewList from './review/ReviewList';
 import HostIndex from './host/HostIndex';
 import InsertQna from './qna/InsertQna';
@@ -16,6 +16,13 @@ import InsertReport from './report/InsertReport';
 import MemModify from './member/MemModify';
 import MemChange from './member/MemChange';
 import MemDelete from './member/MemDelete';
+import ResCancel from './reserve/ResCancel';
+import ResConfirm from './reserve/ResConfirm';
+import MemUseListPage from './reserve/MemUseListPage';
+import Myreview from './review/Myreview';
+import ModifyReview from './review/ModifyReview';
+import DeleteReview from './review/DeleteReview';
+
 
 const MyPageIndex = () => {
   return (
@@ -26,7 +33,8 @@ const MyPageIndex = () => {
         <Link to='member'><li>member</li></Link>
         <Link to='report'><li>report</li></Link>
         <Link to='qna'><li>qna</li></Link>
-        <Link to='reserveList'><li>reserveList</li></Link>
+        <Link to='ReserveListPage'><li>ReserveListPage</li></Link>
+        <Link to='memUseListPage'><li>MemUseListPage</li></Link>
         <Link to='reviewList'><li>reviewList</li></Link>
 
     <Routes>
@@ -46,11 +54,19 @@ const MyPageIndex = () => {
         <Route path='/qna' element={<QnaList/>}/>
         <Route path='/insertQna' element={<InsertQna/>}/>
 
-        <Route path='/reserveList' element={<ReserveList/>}/>
-        <Route path='/useList' element={<MemUseList/>}/>
-        <Route path='/hostDetail' element={<HostDetail/>}/>
+        <Route path='/ReserveListPage' element={<ReserveListPage/>}/>
+        <Route path='/ReserveListPage/resCancel' element={<ResCancel/>}/>
+        <Route path='/ReserveListPage/resConfirm' element={<ResConfirm/>}/>
+        <Route path='/ReserveListPage/hostDetail' element={<HostDetail/>}/>
 
-        <Route path='/insertReview' element={<InsertReview/>}/>
+        <Route path='/memUseListPage' element={<MemUseListPage/>}/>
+        <Route path='/memUseListPage/memUseList' element={<MemUseList/>}/>
+        
+
+        <Route path='/memUseListPage/myreview' element={<Myreview/>}/>
+        <Route path='/memUseListPage/myreview/ModifyReview' element={<ModifyReview/>}/>
+        <Route path='/memUseListPage/myreview/DeleteReview' element={<DeleteReview/>}/>
+        <Route path='/memUseListPage/insertReview' element={<InsertReview/>}/>
         <Route path='/reviewList' element={<ReviewList/>}/>
     </Routes>
     
