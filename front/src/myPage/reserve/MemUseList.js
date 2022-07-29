e577ea47031ee99ae4987776685573523d3a89ef
sa <darkcat90@naver.com>
2022-07-29 10:04:18 +0900

    dd

--- front/src/myPage/reserve/MemUseList.js
+++ front/src/myPage/reserve/MemUseList.js
@@ -6,14 +6,18 @@ const MemUseList = (props) => {
   const {list}=props;
 
   return (
-
-          <div>
-            <h4>예약번호 : {list.RES_IDX}<br/>
-                주소 : {list.BOARD_ADDR}<br/>
-                이용날짜 : {list.RES_DATE_START} ~ {list.RES_DATE_END}<br/>
-                맡긴 동물 수 : {list.RES_CARE_NO}<br/>
-                결제가격 : {list.PRICE} 원
-            </h4>
+          
+          <div class="border border-success p-2 mb-2">
+            <ul class="list-group">
+              <li class="list-group-item">
+                <h4>예약번호 : {list.RES_IDX}<br/>
+                  주소 : {list.BOARD_ADDR}<br/>
+                  이용날짜 : {list.RES_DATE_START} ~ {list.RES_DATE_END}<br/>
+                  맡긴 동물 수 : {list.RES_CARE_NO}<br/>
+                  결제가격 : {list.PRICE} 원
+              </h4>
+              </li>
+            </ul>
           </div>
 
   )
