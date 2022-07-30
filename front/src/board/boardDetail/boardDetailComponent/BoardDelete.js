6ddaa36dcb2bd55d3a0607c2a39c6af7dc0f8577
kimkibon <darkhalu@naver.com>
2022-07-30 23:47:06 +0900

    boardDelete 컴포넌트 누락된 임포트 추가

--- front/src/board/boardDetail/boardDetailComponent/BoardDelete.js
+++ front/src/board/boardDetail/boardDetailComponent/BoardDelete.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 
 const BoardDelete = (props) => {
