c9bb21c927fe09fbfd6a76fd15bae961acec8707
sa <darkcat90@naver.com>
2022-07-30 18:18:55 +0900

    호스트전환(파일빼고)

--- front/src/myPage/member/InsertHost.js
+++ front/src/myPage/member/InsertHost.js
@@ -5,61 +5,20 @@ import InsertHostFiles from './InsertHostFiles';
 
 
 const InsertHost = (props) => {
-  const insertHost = props.insert.insertHost;
-  const insertHostFiles = props.insert.insertHostFiles;
-  
+
   //변수 초기 세팅
+    console.log(props);
+    axios({
+      method : 'post' ,
+      url : '/GareBnB/mypage/memChange.do' ,
+      contentType:"application/json;charset=UTF-8",
+      params : props
 
-  const InsertHost = async (e) => {
-    e.preventDefault();
-    e.persist();
-
-    const files = insertHostFiles.map(file => {
-      let arr = file.url.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]),
-        n = bstr.length,
-        u8arr = new Uint8Array(n);
-
-      while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
-      }
-      return (
-        new File([u8arr], file.fileName, { type: mime })
-      )
-    })
-
-                          await axios({ 
-                            method : 'post',
-                            url : '/GareBnB/mypage/memChange.do', 
-                            params : { 
-                              insertHost
-                            }}).then(async Response => { 
-                              files.map(async (file, index) =>{
-                              await InsertHostFiles(file, Response.data.MEM_IDX , index);
-                              })})
-    
-                            }
-    // 받아온 멤버 인덱스로 이미지 파일을 업로드
+    }).then(Response => {
+        window.location.href="/"
+    });
   
-  return (
-    <Modal
-      {...props}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>글 등록 확인</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>해당 내용으로 글을 작성하시겠습니까?</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
-          취소
-        </Button>
-        <Button type='submit' variant="primary" onClick={async (e) => { InsertHost(e) }}>
-          등록
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  )
+
 }
 
 
--- front/src/myPage/member/MemChange.js
+++ front/src/myPage/member/MemChange.js
@@ -1,137 +1,84 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
 import InsertHost from './InsertHost';
 import ImageUploadBox from '../host/myBoard/component/ImageUploadBox';
 
 const MemChange = () => {
-  const [insertModal, setInsertModal] = React.useState(false);
-  
-  const [insertHostFiles, setInsertHostFiles] = useState([]);
-  const [insertHost, setInsertHost] = useState({
-    'MEM_IDX' : '2',
-    'HOST_EMAIL' : '',
-    'HOST_POSTCODE' : '',
-    'HOST_ADDR1' : '',
-    'HOST_ADDR2' : '',
-    'HOST_JUMIN1' : '',
-		'HOST_JUMIN2' : '',
-		'HOST_INTRO' : '',
-		'HOST_ACCOUNT' : '',
-		'HOST_BANK' : ''
-  });
 
-  const {
+  // MEM_IDX 불러오는 기능있어야함
+
+  const [inputs, setInputs] = useState({    
+    MEM_IDX : 2,
+    HOST_EMAIL : '',
+    HOST_POST : '',
+    HOST_ADDR1 : '',
+    HOST_ADDR2 : '',
+    HOST_JUMIN1 : '',
+		HOST_JUMIN2 : '',
+		HOST_INTRO : '',
+		HOST_ACCOUNT : '',
+		HOST_BANK : ''
+  });  
+
+  const {     
     MEM_IDX,
     HOST_EMAIL,
-    HOST_POSTCODE,
+    HOST_POST,
     HOST_ADDR1,
     HOST_ADDR2,
     HOST_JUMIN1,
     HOST_JUMIN2,
     HOST_INTRO,
     HOST_ACCOUNT,
-    HOST_BANK
-  } = insertHost;
+    HOST_BANK } = inputs;
 
-  const setItems = (e) => {
-    const { name, value } = e.target;
-    setInsertHost({
-      ...insertHost,
-      [name]: value
-    })
-    console.log(insertHost)
+  const onChange = (e) => {    
+    const { value, name } = e.target;  
+    setInputs({      
+      ...inputs, // 기존의 input 객체를 복사한 뒤      
+      [name]: value // name 키를 가진 값을 value 로 설정    
+    });  
   };
-  
 
-  const getImages = (image) => {
-    setInsertHostFiles(image)
-  }
 
   return (
     <div>
-    <h1>호스트 전환하기</h1>
-    <Form>
 
-      <Form.Group className="mb-3">
-        <Form.Label>이메일 주소</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" value={HOST_EMAIL} onChange={(e)=> setItems(e)} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+      <h3>이메일주소 : </h3>
+      <input name="HOST_EMAIL" placeholder="Enter email" onChange={onChange} value={HOST_EMAIL} />
+      <br/>
+      <h3>우편번호 : </h3>
+      <input name="HOST_POST" placeholder="Enter email" onChange={onChange} value={HOST_POST} />
+      <br/>
+      <h3>기본주소 : </h3>
+      <input name="HOST_ADDR1" placeholder="Enter email" onChange={onChange} value={HOST_ADDR1} />
+      <br/>
+      <h3>상세주소 : </h3>
+      <input name="HOST_ADDR2" placeholder="Enter email" onChange={onChange} value={HOST_ADDR2} />
+      <br/>
+      <h3>주민 등록 번호 : </h3>
+      <input name="HOST_JUMIN1" placeholder="Enter email" onChange={onChange} value={HOST_JUMIN1} />
+      <input name="HOST_JUMIN2" placeholder="Enter email" onChange={onChange} value={HOST_JUMIN2} />
+      <br/>
 
-      <Form.Group className="mb-3">
-        <Form.Label>우편번호 <button>검색</button></Form.Label>
-        <Form.Control type="number" placeholder="12345" value={HOST_POSTCODE} onChange={(e)=>setItems(e)} />
-      </Form.Group>
+      <h3>본인 소개 : </h3>
+      <input name="HOST_INTRO" placeholder="Enter email" onChange={onChange} value={HOST_INTRO} />
+      <br/>
+      
 
-      <Form.Group className="mb-3">
-        <Form.Label>기본주소</Form.Label>
-        <Form.Control type="text" placeholder="기본 주소" value={HOST_ADDR1} onChange={(e)=> setItems(e)} />
-      </Form.Group>
+      {/* 사진 첨부 */}
 
-      <Form.Group className="mb-3">
-        <Form.Label>상세주소</Form.Label>
-        <Form.Control type="text" placeholder="상세 주소" value={HOST_ADDR2} onChange={(e)=> setItems(e)} />
-      </Form.Group>
+      <h3>은행 : </h3>
+      <input name="HOST_BANK" placeholder="Enter email" onChange={onChange} value={HOST_BANK} />
+      <br/>
+      <h3>계좌번호 : </h3>
+      <input name="HOST_ACCOUNT" placeholder="Enter email" onChange={onChange} value={HOST_ACCOUNT} />
+      <br/>
 
-      <Form.Group className="mb-3">
-        <Form.Label>주민 등록 번호</Form.Label>
-        <Form.Control type="number" placeholder="주민등록번호 앞자리" value={HOST_JUMIN1} onChange={(e)=> setItems(e)} /> -
-        <Form.Control type="number" placeholder="1" value={HOST_JUMIN2} onChange={(e)=> setItems(e)} /> ******
-      </Form.Group>
-
-      <Form.Group className="mb-3">
-        <Form.Label>본인 사진</Form.Label> <br/>
-        <Form.Text className="text-muted">
-         본인을 확인할 수 있는 사진 첨부 부탁드립니다. (예 : 증명 사진)
-        </Form.Text>
-        <ImageUploadBox getImages={getImages} />
-      </Form.Group>
-
-      <Form.Group className="mb-3">
-        <Form.Label>본인 소개</Form.Label>
-        <Form.Control as="textarea" placeholder="경력, 자격증, 포부, 자기소개" value={HOST_INTRO} onChange={(e)=> setItems(e)} />
-      </Form.Group>
-
-      <Form.Group className="mb-3">
-        <Form.Label>사진 첨부</Form.Label> <br/>
-        <Form.Text className="text-muted">
-         본인 소개글에 대한 증명 자료로 사진이 있으시다면, 호스트 전환에 도움이 될 수 있으므로 추가 부탁드립니다.
-        </Form.Text>
-        <ImageUploadBox getImages={getImages} />
-      </Form.Group>
-
-
-      <Form.Group className="mb-3">
-      <Form.Label>은행</Form.Label>
-      <Form.Select defaultValue={HOST_BANK} onChange={(e)=> setItems(e)}>
-      <option value="default">은행을 선택하여 주세요</option>
-      <option value="신한은행">신한은행</option>
-      <option value="하나은행">하나은행</option>
-      <option value="국민은행">국민은행</option>
-      </Form.Select>
-      </Form.Group>
-
-    <Form.Group className="mb-3">
-        <Form.Label>계좌번호</Form.Label>
-        <Form.Control type="text" placeholder="계좌 번호" defaultValue={HOST_ACCOUNT} onChange={(e)=> setItems(e)} />
-      </Form.Group>
-
-    </Form>
-      <Button variant="primary" onClick={() => setInsertModal(true)}>전환하기</Button> &nbsp;
-      <Button href="/mypage" variant="primary">취소</Button>
-
-      <InsertHost
-          show={insertModal}
-          onHide={() => setInsertModal(false)}
-          insert={{ "insertHost": insertHost, "insertHostFiles": insertHostFiles }}
-        />
-        {/* 입력확인창 모달로 띄우기 !  */}
+      <button onClick={(e)=>{e.preventDefault();InsertHost(inputs);}}>등록하기</button>
+    
     </div>
-    )}
-  
+    
+  )
+}
+
 export default MemChange
