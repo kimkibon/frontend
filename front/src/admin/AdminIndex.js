5adc56bdb66be82d0134a444fc75540bc46d98ef
aram0111 <aramingk@naver.com>
2022-07-30 16:01:35 +0900

    호스트 등록요청 리스트, 상세보기

--- front/src/admin/AdminIndex.js
+++ front/src/admin/AdminIndex.js
@@ -3,8 +3,8 @@ import { Link, Route, Routes } from 'react-router-dom'
 import AdminBoardReview from './adminHostBoard/AdminBoardReview'
 import AdminHostBoardDetail from './adminHostBoard/AdminHostBoardDetail'
 import AdminHostBoardList from './adminHostBoard/AdminHostBoardList'
-import AdminHostDetail from './adminHostMember/AdminHostDetail'
-import AdminHostList from './adminHostMember/AdminHostList'
+import AdminHostConfirmDetail from './adminHostMember/AdminHostConfirmDetail'
+import AdminHostConfirmList from './adminHostMember/AdminHostConfirmList'
 import AdminMemberDetail from './adminMember/AdminMemberDetail'
 import AdminMemberList from './adminMember/AdminMemberList'
 import AdminQnaDetail from './adminQna/AdminQnaDetail'
@@ -24,8 +24,8 @@ const AdminIndex = () => {
       <Link to='adminBoardDetail'><li>adminBoardDetail</li></Link>
       <Link to='adminBoardList'><li>adminBoardList</li></Link>
 
-      <Link to='adminHostList'><li>adminHostList</li></Link>
-      <Link to='adminHostDetail'><li>adminHostDetail</li></Link>
+      <Link to='adminHostConfirmList'><li>adminHostConfirmList</li></Link>
+      <Link to='adminHostConfirmDetail'><li>adminHostConfirmDetail</li></Link>
 
       <Link to='adminMemberList'><li>adminMemberList</li></Link>
       <Link to='adminMemberDetail'><li>adminMemberDetail</li></Link>
@@ -46,8 +46,8 @@ const AdminIndex = () => {
         <Route path='/adminBoardDetail' element={<AdminHostBoardDetail/>}/>
         <Route path='/adminBoardList' element={<AdminHostBoardList/>}/>
 
-        <Route path='/adminHostList' element={<AdminHostList/>}/>
-        <Route path='/adminHostDetail' element={<AdminHostDetail/>}/>
+        <Route path='/adminHostConfirmList' element={<AdminHostConfirmList/>}/>
+        <Route path='/adminHostConfirmDetail/*' element={<AdminHostConfirmDetail/>}/>
 
         <Route path='/adminMemberList' element={<AdminMemberList/>}/>
         <Route path='/adminMemberDetail/*' element={<AdminMemberDetail/>}/>
