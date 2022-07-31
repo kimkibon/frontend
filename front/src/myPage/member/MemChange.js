892d51910673b42211922f1ed2beb0624ab17421
aram0111 <aramingk@naver.com>
2022-07-31 23:37:38 +0900

    host 파일 첨부

--- front/src/myPage/member/MemChange.js
+++ front/src/myPage/member/MemChange.js
@@ -3,8 +3,7 @@ import { Modal } from 'react-bootstrap';
 import ImageUploadBox from '../host/myBoard/component/ImageUploadBox';
 import HostAddress from './HostAddress';
 import axios from 'axios';
-
-
+import InsertHost from './InsertHost';
 
 const MemChange = () => {
 
@@ -14,7 +13,7 @@ const MemChange = () => {
   const [showAddrModal, setShowAddrModal] = React.useState(false);
   const [insertFiles, setInsertFiles] = useState([]);
   const [insertHost, setInsertHost] = useState({    
-    MEM_IDX : '8',
+    MEM_IDX : '18',
     HOST_EMAIL : '',
     HOST_POST : '',
     HOST_ADDR1 : '',
@@ -56,68 +55,55 @@ const MemChange = () => {
     });  
    
   };
-  console.log(insertHost)
-  // const getImages = (image) => {
-  //   setInsertHostFiles(image)
-  // }
-  //  // 미리보기로 만들어진 이미지를 저장 
 
-  const memChange = () => {
-          if(HOST_EMAIL!==""){
-            if(HOST_POST!==""){
-              if(HOST_ADDR1!==""){
-                if(HOST_ADDR2!==""){
-                  if(HOST_JUMIN1!==""){
-                    if(HOST_JUMIN2!==""){
-                      if(HOST_INTRO!==""){
-                        if(HOST_ACCOUNT!==""){
-                          if(HOST_BANK!==""){
-              axios({
-                method : 'post' ,
-                url : '/GareBnB/mypage/memChange.do' ,
-                contentType:"application/json;charset=UTF-8",
-                params : insertHost
-                }).then(Response => {
-                  alert("성공")
-                  // 메인으로 나가든가 어디로든 나가든가 
-                }).catch(err => {
-                  console.log(err);
-                });
-                    }
-                    else alert("은행명을 입력해주세요")
-                  }
-                  else alert("계좌번호를 입력해주세요")
-                }
-                else alert("소개글을 입력해주세요")
-              }
-              else alert("주민등록번호 뒷자리(1자리)를 입력해주세요")
-            }
-            else alert("주민등록번호 앞자리를 입력해주세요")
-          }
-          else alert("상세 주소를 입력해주세요")
-        }
-        else alert("기본 주소를 입력해주세요")
-      }
-      else alert("우편번호를 입력해주세요")
-    }
-    else alert("이메일을 입력해주세요")
+  const getImages = (image) => {
+    setInsertFiles(image)
   }
+   // 미리보기로 만들어진 이미지를 저장 
 
-  const HostLevUpdate=() => { // 호스트 전환 요청시 레벨 2 -> 3으로 업데이트
-  axios({
-    method : 'post' ,
-    url : '/GareBnB/mypage/updateHostMem.do' ,
-    contentType:"application/json;charset=UTF-8",
-    params : {
-      MEM_IDX : insertHost.MEM_IDX
-    }}).then(Response => {
-      alert("성공")
-      // 메인으로 나가든가 어디로든 나가든가 
-    }).catch(err => {
-      console.log(err);
-    });
+   const insertOnClick = () => {
+
+    if (insertHost.HOST_EMAIL === '') {
+      alert('이메일을 입력해주세요.')
+    } else {
+      if (insertHost.HOST_POST === '') {
+        alert('우편번호를 입력해주세요..')
+      } else {
+        if (insertHost.HOST_ADDR1 === '') {
+          alert('주소를 입력해주세요.')
+        } else {
+          if (insertHost.HOST_ADDR2 === '') {
+            alert('상세주소를 입력해주세요.')
+          } else {
+            if (insertHost.HOST_JUMIN1 === '') {
+              alert('주민등록번호 앞자리를 입력해주세요.')
+            } else {
+              if (insertHost.HOST_JUMIN2 === '') {
+                alert('주민등록번호 뒷자리(1자리)를 입력해주세요.')
+              } else {
+                if (insertHost.HOST_INTRO === '') {
+                  alert('소개글을 입력해주세요.')
+                } else {
+                  if (insertHost.HOST_BANK === '') {
+                    alert('은행명을 입력해주세요.')
+                  } else {
+                    if (insertHost.HOST_ACCOUNT === '') {
+                      alert('계좌번호를 입력해주세요.')
+                    } else {
+                if (insertFiles[1] === undefined) {
+                  alert('사진을 두 장 이상 입력해주세요')
+                } else {
+                  setInsertModal(true);
+                }
+              }
+            }
+          }
         }
-      
+      }
+    }
+  }
+}
+    }}
 
   return (
     <div>
@@ -143,6 +129,9 @@ const MemChange = () => {
       <input name="HOST_JUMIN1" placeholder="주민 등록 번호 앞자리" onChange={onChange} value={HOST_JUMIN1} /> -  
       <input name="HOST_JUMIN2" placeholder="주민 등록 번호 뒷자리 1번째" onChange={onChange} value={HOST_JUMIN2} /> ******
       <br/>
+      <h3>호스트 사진 : </h3>
+          {/* 사진 첨부 */}
+      <ImageUploadBox getImages={getImages} />
       <h3>본인 소개 : </h3>
       <input name="HOST_INTRO" placeholder="본인 소개" onChange={onChange} value={HOST_INTRO} />
       <br/>
@@ -154,25 +143,24 @@ const MemChange = () => {
       <input name="HOST_ACCOUNT" placeholder="계좌번호 입력" onChange={onChange} value={HOST_ACCOUNT} />
       <br/>
 
-        {/* 입력확인창 모달로 띄우기 !  */}
+      <InsertHost
+          show={insertModal}
+          onHide={() => setInsertModal(false)}
+          props={{
+            'insertHost': insertHost,
+            'insertFiles': insertFiles,
+            'fileType': '1',
+          }}/>
+
+        {/* 입력확인창 모달로 띄우기 !  */} {/* 주소 검색 모달 */}
         <Modal
           show={showAddrModal}
-          onHide={() => setShowAddrModal(false)}
-        >
+          onHide={() => setShowAddrModal(false)}>
           <HostAddress
-            setAddrInfo={setAddrInfo}
-          />
+            setAddrInfo={setAddrInfo}/>
         </Modal>
-        {/* 주소 검색 모달 */}
-
-      {/* 사진 첨부 */}
-
-
-      <button onClick={ () => { // 버튼 클릭하면 호스트 DB에 저장 & 멤버 DB에 레벨 3으로 업데이트 
-        memChange()
-        HostLevUpdate()
-      }}>등록하기</button>
-    
+ 
+    <button onClick={insertOnClick}>등록하기</button>
     </div>
     
   )
