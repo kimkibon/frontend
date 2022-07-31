10bd120b5f815551b8112735ae8d35b3f8676c2e
aram0111 <aramingk@naver.com>
2022-07-31 19:52:15 +0900

    흐엉엉

--- front/src/myPage/member/MemChange.js
+++ front/src/myPage/member/MemChange.js
@@ -5,16 +5,16 @@ import HostAddress from './HostAddress';
 import axios from 'axios';
 
 
+
 const MemChange = () => {
 
   // MEM_IDX 불러오는 기능있어야함
 
   const [insertModal, setInsertModal] = React.useState(false);
   const [showAddrModal, setShowAddrModal] = React.useState(false);
-  const [insertHostFiles, setInsertHostFiles] = useState([]);
-
+  const [insertFiles, setInsertFiles] = useState([]);
   const [insertHost, setInsertHost] = useState({    
-    MEM_IDX : 6,
+    MEM_IDX : '9',
     HOST_EMAIL : '',
     HOST_POST : '',
     HOST_ADDR1 : '',
@@ -47,7 +47,6 @@ const MemChange = () => {
       setShowAddrModal(false)
       console.log(insertHost)
     }
-  
 
   const onChange = (e) => {    
     const { value, name } = e.target;  
@@ -55,97 +54,105 @@ const MemChange = () => {
       ...insertHost, // 기존의 input 객체를 복사한 뒤      
       [name]: value // name 키를 가진 값을 value 로 설정    
     });  
-    console.log(insertHost)
+   
   };
+  console.log(insertHost)
   // const getImages = (image) => {
   //   setInsertHostFiles(image)
   // }
   //  // 미리보기로 만들어진 이미지를 저장 
 
-//    const insertOnClick = () => {
+  const memChange = () => {
+          if(HOST_EMAIL!==""){
+            if(HOST_POST!==""){
+              if(HOST_ADDR1!==""){
+                if(HOST_ADDR2!==""){
+                  if(HOST_JUMIN1!==""){
+                    if(HOST_JUMIN2!==""){
+                      if(HOST_INTRO!==""){
+                        if(HOST_ACCOUNT!==""){
+                          if(HOST_BANK!==""){
+              axios({
+                method : 'post' ,
+                url : '/GareBnB/mypage/memChange.do' ,
+                contentType:"application/json;charset=UTF-8",
+                params : {
+                  insertHost
+                }}).then(Response => {
+                  alert("성공")
+                  // 메인으로 나가든가 어디로든 나가든가 
+                }).catch(err => {
+                  console.log(err);
+                });
+                    }
+                    else alert("은행명을 입력해주세요")
+                  }
+                  else alert("계좌번호를 입력해주세요")
+                }
+                else alert("소개글을 입력해주세요")
+              }
+              else alert("주민등록번호 뒷자리(1자리)를 입력해주세요")
+            }
+            else alert("주민등록번호 앞자리를 입력해주세요")
+          }
+          else alert("상세 주소를 입력해주세요")
+        }
+        else alert("기본 주소를 입력해주세요")
+      }
+      else alert("우편번호를 입력해주세요")
+    }
+    else alert("이메일을 입력해주세요")
+  }
 
-//     if (insertHost.HOST_EMAIL === '') {
-//       alert('제목을 입력해주세요.')
-//     } else {
-//       if (insertHost.HOST_POST === '') {
-//         alert('우편번호를 입력해주세요..')
-//       } else {
-//         if (insertHost.HOST_ADDR1 === '') {
-//           alert('주소를 입력해주세요.')
-//         } else {
-//           if (insertHost.HOST_ADDR2 === '') {
-//             alert('상세주소를 입력해주세요.')
-//           } else {
-//             if (insertHost.HOST_JUMIN1 === '') {
-//               alert('가격을 입력해주세요.')
-//             } else {
-//               if (insertHost.HOST_JUMIN2 === '') {
-//                 alert('소개글을 입력해주세요.')
-//               } else {
-//                 if (insertHost.HOST_INTRO === '') {
-//                   alert('소개글을 입력해주세요.')
-//                 } else {
-//                   if (insertHost.HOST_BANK === '') {
-//                     alert('소개글을 입력해주세요.')
-//                   } else {
-//                     if (insertHost.HOST_ACCOUNT === '') {
-//                       alert('소개글을 입력해주세요.')
-//                     } else {
-//                 if (insertHostFiles[1] === undefined) {
-//                   alert('사진을 두 장 이상 입력해주세요')
-//                 } else {
-//                   setInsertModal(true);
-//                 }
-//               }
-//             }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-  // 글 유효성 검사  
-
-  const Send = () => {
+  const HostLevUpdate=() => { // 호스트 전환 요청시 레벨 2 -> 3으로 업데이트
   axios({
     method : 'post' ,
-    url : '/GareBnB/mypage/memChange.do' ,
+    url : '/GareBnB/mypage/updateHostMem.do' ,
     contentType:"application/json;charset=UTF-8",
-    params : insertHost
-  //insertHost 리턴으로 MEM_IDX를 받아옴
-  })}
+    params : {
+      MEM_IDX : insertHost.MEM_IDX
+    }}).then(Response => {
+      alert("성공")
+      // 메인으로 나가든가 어디로든 나가든가 
+    }).catch(err => {
+      console.log(err);
+    });
+        }
+      
 
   return (
     <div>
      <h3>이메일주소 : </h3>
-      <input name="HOST_EMAIL" placeholder="Enter email" onChange={onChange} value={HOST_EMAIL} />
+      <input name="HOST_EMAIL" placeholder="이메일 입력" onChange={onChange} value={HOST_EMAIL} />
       <br/>
-   
-     
       <h3>우편번호 : </h3>
-      <input name="HOST_POST" placeholder="Enter POST" onChange={onChange} value={HOST_POST} />
+      <input name="HOST_POST" placeholder="우편번호 입력" onChange={onChange} value={HOST_POST} />
       <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="button-addon"
-            onClick={() => setShowAddrModal(true)}>
-            우편번호 찾기
+      className="btn btn-outline-secondary"
+      type="button"
+      id="button-addon"
+      onClick={() => setShowAddrModal(true)}>
+      우편번호 찾기
       </button>
       <h3>기본주소 : </h3>
-      <input name="HOST_ADDR1" placeholder="Enter email" onChange={onChange} value={HOST_ADDR1} />
+      <input name="HOST_ADDR1" placeholder="기본 주소 입력" onChange={onChange} value={HOST_ADDR1} />
       <br/>
       <h3>상세주소 : </h3>
-      <input name="HOST_ADDR2" placeholder="Enter email" onChange={onChange} value={HOST_ADDR2} />
+      <input name="HOST_ADDR2" placeholder="상세 주소 입력" onChange={onChange} value={HOST_ADDR2} />
       <br/>
       <h3>주민 등록 번호 : </h3>
-      <input name="HOST_JUMIN1" placeholder="Enter email" onChange={onChange} value={HOST_JUMIN1} /> -  
-      <input name="HOST_JUMIN2" placeholder="Enter email" onChange={onChange} value={HOST_JUMIN2} />
+      <input name="HOST_JUMIN1" placeholder="주민 등록 번호 앞자리" onChange={onChange} value={HOST_JUMIN1} /> -  
+      <input name="HOST_JUMIN2" placeholder="주민 등록 번호 뒷자리 1번째" onChange={onChange} value={HOST_JUMIN2} /> ******
       <br/>
-
       <h3>본인 소개 : </h3>
-      <input name="HOST_INTRO" placeholder="Enter email" onChange={onChange} value={HOST_INTRO} />
+      <input name="HOST_INTRO" placeholder="본인 소개" onChange={onChange} value={HOST_INTRO} />
+      <br/>
+      <h3>은행 : </h3>
+      {/* 은행명 선택할 수 있게 수정하기 */}
+      <input name="HOST_BANK" placeholder="은행명" onChange={onChange} value={HOST_BANK} />
+      <br/>
+      <h3>계좌번호 : </h3>
+      <input name="HOST_ACCOUNT" placeholder="계좌번호 입력" onChange={onChange} value={HOST_ACCOUNT} />
       <br/>
 
         {/* 입력확인창 모달로 띄우기 !  */}
@@ -161,14 +168,11 @@ const MemChange = () => {
 
       {/* 사진 첨부 */}
 
-      <h3>은행 : </h3>
-      <input name="HOST_BANK" placeholder="Enter email" onChange={onChange} value={HOST_BANK} />
-      <br/>
-      <h3>계좌번호 : </h3>
-      <input name="HOST_ACCOUNT" placeholder="Enter email" onChange={onChange} value={HOST_ACCOUNT} />
-      <br/>
 
-      <button onClick={Send}>등록하기</button>
+      <button onClick={ () => { // 버튼 클릭하면 호스트 DB에 저장 & 멤버 DB에 레벨 3으로 업데이트 
+        memChange()
+        HostLevUpdate()
+      }}>등록하기</button>
     
     </div>
     
