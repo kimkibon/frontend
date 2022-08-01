9a7717a5e4e53f7ff4b8490d8315bb838fc858a0
hoyeon <hoyeon9353@naver.com>
2022-08-01 15:54:38 +0900

    0801

--- front/src/admin/AdminIndex.js
+++ front/src/admin/AdminIndex.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
+import AdminSidebar from '../commons/AdminSidebar'
 import AdminBoardReview from './adminHostBoard/AdminBoardReview'
 import AdminHostBoardDetail from './adminHostBoard/AdminHostBoardDetail'
 import AdminHostBoardList from './adminHostBoard/AdminHostBoardList'
@@ -17,7 +18,14 @@ import ResCancel from '../myPage/reserve/ResCancel';
 
 const AdminIndex = () => {
   return (
-    <div>
+      <div class="container-fluid">
+      <div class="row">
+        
+        <div class='col-lg-2'>
+          <AdminSidebar />
+        </div>
+
+        <div className='col-lg-8'>
       <Link to=''><li>admin index</li></Link>
       
       <Link to='adminBoardReview'><li>adminBoardReview</li></Link>
@@ -39,7 +47,7 @@ const AdminIndex = () => {
       <Link to='adminAllResList'><li>adminAllResList</li></Link>
 
       
-
+      
       <Routes>
         <Route path='/admin' element={<AdminIndex/>}/>
         <Route path='/adminBoardReview' element={<AdminBoardReview/>}/>
@@ -64,7 +72,13 @@ const AdminIndex = () => {
     
 
       </Routes>
-      
+      </div>
+
+        <div class='col-lg-2'/>
+        
+
+
+      </div>
     </div>
   )
 }
