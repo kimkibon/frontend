9f8cb6f9a3aa423b5e7abbd4a1b16f7622d84fe1
kimkibon <darkhalu@naver.com>
2022-08-01 14:07:50 +0900

    주석 빼벅어서 주석 달았습니다.

--- front/src/login/Auth.js
+++ front/src/login/Auth.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 async function Auth(BOARD_LEVEL , navigate) {
 
+    // BOARD_LEVEL 의 접근권한 
+    // 0 = 관리자 
+    // 1 = 호스트 
+    // 2 = 일반회원
+    // 3 = 호스트 대기중 일반회원
+    // 4 = 호스트 거절된 일반회워 
+    // 5 = 탈퇴한 회원
+    // 6 = 정지된 회원 
+
     return (await new Promise((resolve, reject) => {
         const id = localStorage.getItem("MEM_ID");
 
