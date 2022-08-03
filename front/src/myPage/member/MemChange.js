2c12b844b5fb6fc857390837f75a5f37379e2ce0
aram0111 <aramingk@naver.com>
2022-08-03 14:31:49 +0900

    INFO

--- front/src/myPage/member/MemChange.js
+++ front/src/myPage/member/MemChange.js
@@ -108,7 +108,9 @@ const MemChange = () => {
   }
 
   return (
-    <div>
+       <div className="container">
+      <section className="row border border-primary">
+      <div className="col-md-6">
       <h2>호스트 전환하기</h2>
       <div>
       <Form.Group>
