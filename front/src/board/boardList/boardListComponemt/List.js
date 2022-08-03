2e116944828a7a9432a3864ccf4eddf5f8d7a287
kimkibon <darkhalu@naver.com>
2022-08-04 00:26:59 +0900

    메인 리스트 ui 수정

--- front/src/board/boardList/boardListComponemt/List.js
+++ front/src/board/boardList/boardListComponemt/List.js
@@ -2,57 +2,67 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const List = (board) => {
 
-//상위 컴포넌트에서 받아온 데이터를 표시 
+  //상위 컴포넌트에서 받아온 데이터를 표시 
 
   return (
+    <div className="container">
 
-    <div className="container border border-primary">
-      {board[0] !== undefined && board.map(list => {
-        return (
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {board[0] !== undefined && board.map(list => {
+          return (
 
-          <div className="row border border-primary" key={list.BOARD_NO}>
-            <div className="col-md-7">
-              <img className="d-block w-100" 
-                   width='700px'
-                   height='300px'
-                    
-                   src={list.URL} 
-                   alt="" 
-                   />
+
+
+            <div className="col">
+              <div className="card shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                >
+                  <title>Placeholder</title>
+                  <img
+                    className="d-block w-100"
+                    src={list.URL}
+                    alt=""
+                  />
+                </svg>
+
+                <div className="card-body">
+                  <h4 className="card-text">
+                    {list.BOARD_TITLE}
+                  </h4>
+                  <figure class="text-end">
+                    <p>
+                      {list.BOARD_PRICE}원/일
+                    </p>
+                  </figure>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <Link to='Detail' state={list}>
+                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      </Link>
+                    </div>
+                    <small className="text-muted">{list.BOARD_SCORE}</small>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="col-md-4">
-              <h3>{list.BOARD_TITLE}</h3>
-              <p>{list.BOARD_CONTENT}</p>
-              <h5 className='text-end'>{list.BOARD_PRICE}원/일</h5>
-              <h5 className='text-end'>{list.BOARD_SCORE}</h5>
-              <Link to='Detail' state={list}>
-                <button className="btn btn-primary" href="#">자세한 정보 알아보기</button>
-              </Link>
-            </div>
-          </div>
 
-        )
-      })}
 
-{/* example */}
-      <div className="row">
-        <div className="col-md-7">
-          <img className="d-block w-100" src="https://via.placeholder.com/700x300" alt="" />
 
-        </div>
-        <div className="col-md-5">
-          <h3>Project One</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          <h5 className='text-end'>PRICE</h5>
-          <h5 className='text-end'>SCORE</h5>
-          <div className='col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0'>
-          <button className="btn btn-primary" href="#">자세한 정보 알아보기</button>
-          </div>
-        </div>
+          )
+        })}
 
+        {/* example */}
+
+        {/*  */}
       </div>
-{/*  */}
-
     </div>
 
   )
