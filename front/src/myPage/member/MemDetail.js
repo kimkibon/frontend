eda9e12c66929910c75fe78d238095f8a30d6675
aram0111 <aramingk@naver.com>
2022-08-04 14:32:57 +0900

    회원탈퇴 모달수정

--- front/src/myPage/member/MemDetail.js
+++ front/src/myPage/member/MemDetail.js
@@ -3,16 +3,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-import Modal from '../member/Modal';
+import { Button } from 'react-bootstrap';
 import Auth from '../../login/Auth';
+import { Modal } from 'react-bootstrap';
 
 const MemDetail = () => {
-    const navigate = useNavigate();
+    const Navigate = useNavigate();
     const [author, setAuthor] = useState({});
     const [memDetail, setMemDetail] = useState([]);
+    const [insertModal, setInsertModal] = React.useState(false);
+    const [memDelete, setmemDelete] = useState([]);
+    const [originPw, setOriginPw] = useState([]);
+    const [show, setShow] = useState(false);
 
     useEffect(() => {// 회원정보 보여주기
-        Auth(4, navigate).then(Response => {
+        Auth(4, Navigate).then(Response => {
             setAuthor(Response)
             console.log(author)
             axios({
@@ -30,36 +35,8 @@ const MemDetail = () => {
         })// auth
     }, []);
 
-    const [memDelete, setmemDelete] = useState([]); // 탈퇴 회원으로 레벨 업데이트 
-    const onDeleteMem = (mem_id) => {
-        axios({
-            method: 'post',
-            url: '/GareBnB/mypage/memDelete.do',
-            contentType: "application/json; charset=UTF-8",
-            params: {
-                MEM_ID: mem_id
-            }
-        })
-            .then(Response => {
-                console.log(Response.data);
-                setmemDelete(Response.data);
-            })
-    };
-
-
-    const [modalOpen, setModalOpen] = useState(false); //  모달 오픈 상태 (true: 열림, false : 닫힘) - 비밀번호 입력하는 모달
-    const openModal = () => {
-        setModalOpen(true);
-    };
-    const closeModal = () => {
-        setModalOpen(false);
-    };
-
-
-    const [originPW, setOriginPw] = useState(""); // 탈퇴할 때 입력하는 비밀번호 
-
     const PWCHECK = () => {
-        if (originPW === memDetail.MEM_PW) { // 비밀번호와 입력한 비밀번호가 일치하면 회원 탈퇴 페이지로 이동
+        if (originPw === memDetail.MEM_PW) { // 비밀번호와 입력한 비밀번호가 일치하면 회원 탈퇴 진행
             alert("탈퇴가 완료되었습니다")
             onDeleteMem(memDetail.MEM_ID) // 탈퇴 진행 (레벨 6으로 업데이트)
             window.location.href = '../index/' // 메인 페이지로 이동 
@@ -69,6 +46,30 @@ const MemDetail = () => {
         }
     }
 
+    const onDeleteMem = () => { // 회원 탈퇴
+        axios({
+            method: 'post',
+            url: '/GareBnB/mypage/memDelete.do',
+            contentType: "application/json; charset=UTF-8",
+            params: {
+                MEM_ID: localStorage.MEM_ID
+            }
+        })
+            .then(Response => {
+                console.log(Response.data);
+                setmemDelete(Response.data);
+                // 메인페이지로 이동하는 링크 작성!!!
+            })
+    };
+
+    const handleShow = () => { // 모달창 열림
+        setShow(true);
+    }
+    const handleClose = () => { // 모달창 닫힘
+        setShow(false);
+        // 메인페이지로 이동하는 링크 작성!!!!!
+    }
+
 
     return (
 
@@ -76,41 +77,61 @@ const MemDetail = () => {
             <h1>회원정보 보기</h1>
             <div className='row'>
                 <label for="staticEmail" class="col-md-4 col-form-label">아이디</label>
-                    <div className='col-md-8'>
-                        <input type='text' value={memDetail.MEM_ID} class="form-control"></input></div>
+                <div className='col-md-8'>
+                    <input type='text' value={memDetail.MEM_ID} class="form-control"></input></div>
             </div>
 
             <div className='row'>
                 <label for="staticEmail" class="col-md-4 col-form-label">이름</label>
-                    <div className='col-md-8'>
-                        <input type='text' value={memDetail.MEM_NAME} class="form-control"></input></div>
+                <div className='col-md-8'>
+                    <input type='text' value={memDetail.MEM_NAME} class="form-control"></input></div>
             </div>
 
             <div className='row'>
                 <label for="staticEmail" class="col-md-4 col-form-label">비밀번호(임시)</label>
-                    <div className='col-md-8'>
-                        <input type='text' value={memDetail.MEM_PW} class="form-control"></input></div>
+                <div className='col-md-8'>
+                    <input type='text' value={memDetail.MEM_PW} class="form-control"></input></div>
             </div>
 
             <div className='row'>
                 <label for="staticEmail" class="col-md-4 col-form-label">휴대폰 번호</label>
-                    <div className='col-md-8'>
-                        <input type='text' value={memDetail.MEM_PHONE} class="form-control"></input></div>
+                <div className='col-md-8'>
+                    <input type='text' value={memDetail.MEM_PHONE} class="form-control"></input></div>
             </div>
 
-                <Link to={'../member/MemModify'} state={{ mem: memDetail }}><button>수정하기</button></Link>
-                &nbsp; &nbsp; &nbsp; &nbsp;
+            <div class="row">
+                <div className='col-lg-6 col-sm-12 text-lg-start'>
+                    <Button className="btn btn-light" type="button" >
+                        <Link to={'../member/MemModify'} style={{ textDecoration: "none" }} state={{ mem: memDetail }}>수정하기</Link>
 
-                <button onClick={openModal}>탈퇴하기</button>
+                    </Button>
+                </div>
+                <div className='col-lg-6 col-sm-12 text-lg-end'>
+                    <Button className="btn btn-light" type="button" onClick={handleShow}>
+                        탈퇴하기
+                    </Button>
+                </div>
 
-                <Modal open={modalOpen} close={closeModal}>
-                    <h3>비밀번호 입력 : <input type="password" placeholder='비밀번호를 입력하세요' onChange={e => setOriginPw(e.target.value)} ></input></h3>
-                    <button onClick={PWCHECK}>확인</button> &nbsp; &nbsp; &nbsp; &nbsp;
-                    <button onClick={() => setModalOpen(false)}>취소</button>
+
+                <Modal show={show}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>정말로 탈퇴하시겠습니까?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        비밀번호를 입력하신 후, 탈퇴 버튼을 클릭하시면 탈퇴 처리가 완료됩니다.
+                        <input type="password" placeholder='비밀번호를 입력하세요' onChange={e => setOriginPw(e.target.value)} >
+                        </input>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={PWCHECK}>탈퇴</Button>
+                        <Button variant="secondary" onClick={handleClose}>취소</Button>
+                    </Modal.Footer>
                 </Modal>
 
-              
-            </div> 
+
+            </div>
+        </div>
+
     )
 }
 
