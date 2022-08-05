01b516d19db0d33ecbb7c60b47052939eb2b7510
shoot9714 <shoot3957@daum.net>
2022-08-05 14:14:07 +0900

    login

--- front/src/login/LoginIndex.js
+++ front/src/login/LoginIndex.js
@@ -5,30 +5,25 @@ import FindIdResult from './findInfo/FindIdResult'
 import FindPw from './findInfo/FindPw'
 import FindPwResult from './findInfo/FindPwResult'
 import Login from './LoginPage'
-import Logintest from './logintest'
-import { Navigate, useNavigate } from 'react-router-dom';
+
 
 
 const LoginIndex = () => {
   
 
   return (
-    <div>
    
-   <Link to='LoginPage'><li>LoginPage</li></Link>
-    <Link to='findId'><li>findId</li></Link>
-    <Link to='findIdResult'><li>findIdResult</li></Link>
-    <Link to='findPw'><li>findPw</li></Link>
-    <Link to='findPwResult'><li>findPwResult</li></Link>
+  
    
     <Routes>
+    <Route path='/' element={<Login/>}/>
     <Route path='/loginPage' element = {<Login/>}/>
     <Route path='/findId' element = {<FindId/>}/>
     <Route path='/findPw' element ={<FindPw/>}/>
     <Route path='/findIdResult' element ={<FindIdResult/>}/>
     <Route path='/findPwResult' element ={<FindPwResult/>}/>
     </Routes>
-    </div>
+   
   )
 }
 
