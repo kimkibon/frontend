eff3f7e21a5d72fc74dc0cfce9ec17742530cd8f
shoot9714 <shoot3957@daum.net>
2022-08-05 18:31:40 +0900

    findPW

--- front/src/login/findInfo/FindIdResult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FindIdResult = () => {
-  return (
-    <div>안쓰는화면</div>
-  )
-}
-
-export default FindIdResult
--- front/src/login/findInfo/FindPwResult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FindPwResult = () => {
-  return (
-    <div>안쓰는화면2</div>
-  )
-}
-
-export default FindPwResult
