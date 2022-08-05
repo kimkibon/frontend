00bf9912f15e71df1962e2b898fe7d8d3aa0018c
kimkibon <darkhalu@naver.com>
2022-08-05 12:38:05 +0900

    리뷰 삭제 추가

--- front/src/board/boardDetail/boardDetailComponent/Review.js
+++ front/src/board/boardDetail/boardDetailComponent/Review.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import React from 'react';
 
 //board_detail 에서 받아온 리뷰 정보를 출력하는 컴포넌트 
 
 const Review = (review) => {
   // boardDetail 컴포넌트에서 변수를 받아옴. 
+
+  const deleteReview = (reviewIDX) => {
+    axios({
+      method: 'post',
+      url: 'GareBnB/Admin/deleteReview.do',
+      params: {
+        'REVIEW_IDX': reviewIDX
+      }
+    }).then(
+      alert('리뷰가 삭제되었습니다.')
+    )
+  }
+
   return (
     <div className="container px-4 px-lg-5 mt-5">
       <h2 className="fw-bolder mb-4">Reviews</h2>
@@ -11,31 +25,38 @@ const Review = (review) => {
         {review.prop[0] !== undefined && review.prop.map(view => {
           return (
             <div className="col mb-6">
-            <div className="card h-100">
-              <div className="card-body p-4">
-                <div className='row'>
-                  <div className='col-sm-9'>
-                    <div className="text-left">
-                      <h5 className="fw-bolder">{view.REVIEW_MEM_ID}</h5>
+              <div className="card h-100">
+                <div className="card-body p-4">
+                  <div className='row'>
+                    <div className='col-sm-9'>
+                      <div className="text-left">
+                        <h5 className="fw-bolder">{view.REVIEW_MEM_ID}</h5>
+                      </div>
+                    </div>
+                    <div className='col sm-3'>
+                      <span className='text-right'>{view.REVIEW_DATE}</span>
+                      <div>{view.SCORE}</div>
                     </div>
                   </div>
-                  <div className='col sm-3'>
-                    <span className='text-right'>{view.REVIEW_DATE}</span>
-                    <div>{view.SCORE}</div>
+                  <div className='row'>
+                    <div><br /></div>
+                    <div className='col col-sm-11'>
+                      <span>{view.REVIEW_CONTENT}</span>
+                    </div>
+                    {review.auth.MEM_LEVEL === 0 &&
+                      <div className='col col-sm-1'>
+                        <button className='btn - btn-danger' onClick={() => deleteReview(view.REVIEW_IDX)}>
+                          삭제
+                        </button>
+                      </div>
+                    }
                   </div>
                 </div>
-                <div className='row'>
-                  <div><br/></div>
-                  <div className='col'>
-                    <span>{view.REVIEW_CONTENT}</span>
-                  </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 </div>
               </div>
-              <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-              </div>
-            </div>
 
-          </div>
+            </div>
           )
         })}
         {review.prop[0] === undefined &&
@@ -49,14 +70,20 @@ const Review = (review) => {
                     </div>
                   </div>
                   <div className='col sm-3'>
-                    <span className='text-right'>날짜 , 별점</span>
+                    <p className='text-right'>날짜 </p><p> 별점</p>
                   </div>
                 </div>
                 <div className='row'>
-                  <div><br/></div>
                   <div className='col'>
                     <span>첫 리뷰를 작성해주세요!</span>
                   </div>
+                  {review.auth.MEM_LEVEL === 0 &&
+                    <div className='col col-sm-1'>
+                      <button className='btn - btn-danger' >
+                        삭제
+                      </button>
+                    </div>
+                  }
                 </div>
               </div>
               <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
--- front/src/commons/Sidebar.js
+++ front/src/commons/Sidebar.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './sidestyle.css'
-import { BsFillAlarmFill, BsFillCalendarCheckFill, BsFillMegaphoneFill, BsFillQuestionCircleFill,BsFillInfoCircleFill,BsFillPeopleFill  } from "react-icons/bs";
+import { BsFillAlarmFill, BsFillCalendarCheckFill, BsFillMegaphoneFill, BsFillQuestionCircleFill, BsFillInfoCircleFill, BsFillPeopleFill } from "react-icons/bs";
 import { Link, useNavigate } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/js/bootstrap'
 import HostSidebar from './HostSidebar';
 import Auth from '../login/Auth';
 import { Col } from 'react-bootstrap';
+import AdminSidebar from './AdminSidebar';
 
 
 // react-icons names -> fa로 통일
@@ -18,84 +19,85 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   const [memberLevel, setMemberLevel] = useState({
-    MEM_LEVEL:''
+    MEM_LEVEL: ''
   });
   console.log(mem_id);
 
-  const {MEM_LEVEL} = memberLevel;
-  
- 
+  const { MEM_LEVEL } = memberLevel;
+
+
   //level 2 -> 호스트,,,,
   useEffect(() => { // 레벨 4 이하인(일반,호스트,관리자) 접근 가능. MEM_IDX 받아오기
-    if(localStorage.getItem('MEM_ID')){
-    Auth(4 , navigate).then(Res => {
-          setMemberLevel({
-            ...memberLevel,
-            'MEM_LEVEL': Res.MEM_LEVEL,
-            })
-          })
-        }
+    if (localStorage.getItem('MEM_ID')) {
+      Auth(4, navigate).then(Res => {
+        setMemberLevel({
+          ...memberLevel,
+          'MEM_LEVEL': Res.MEM_LEVEL,
+        })
+      })
+    }
   }, []);
   console.log(memberLevel);
 
-  
+
 
   return (
     <Col>
-        <div className='navigation'>
-          <ul className='big_menu1'>
+      <div className='navigation'>
+        <ul className='big_menu1'>
           <li>
-              <Link to='/myPage/ReserveListPage'>
-                <span className='icon'><BsFillAlarmFill className='fa'/></span>
-                <span className='title'>RESERVATION</span>
-              </Link>
-            </li>
-
-              <li>
-              <Link to="/myPage/memUseListPage">
-                <span className='icon'><BsFillCalendarCheckFill className='fa' /></span>
-                <span className='title'>이용내역</span>
-              </Link>
-              </li>
-
-              <li>
-              <Link to="/myPage/qna">
-                <span className='icon'><BsFillQuestionCircleFill className='fa'/></span>
-                <span className='title'> QNA </span>
-              </Link>
-              </li>
-
-              <li>
-              <Link to="/myPage/report">
-                <span className='icon'><BsFillMegaphoneFill className='fa'/></span>
-                <span className='title'>REPORT</span>
-              </Link>
-              </li>
-
-              <li>
-              <Link to="/myPage/member/MemDetail">
-                <span className='icon'><BsFillInfoCircleFill className='fa'/></span>
-                <span className='title'>회원정보보기</span>
-              </Link>
-              </li>
-
-              <li>
-              <Link to="/myPage/member/MemChange">
-                <span className='icon'><BsFillPeopleFill className='fa'/></span>
-                <span className='title'>호스트전환</span>
-              </Link>
+            <Link to='/myPage/ReserveListPage'>
+              <span className='icon'><BsFillAlarmFill className='fa' /></span>
+              <span className='title'>RESERVATION</span>
+            </Link>
           </li>
 
-          </ul>
+          <li>
+            <Link to="/myPage/memUseListPage">
+              <span className='icon'><BsFillCalendarCheckFill className='fa' /></span>
+              <span className='title'>이용내역</span>
+            </Link>
+          </li>
 
-          {/* ----------------------------------------------------------------------------------------- */}
-          
-          {MEM_LEVEL === 1 && <HostSidebar/>}
-        </div>
+          <li>
+            <Link to="/myPage/qna">
+              <span className='icon'><BsFillQuestionCircleFill className='fa' /></span>
+              <span className='title'> QNA </span>
+            </Link>
+          </li>
 
-        {/* <div class="toggle"><AiOutlineMenu className='fa' onClick={toggleMenu()}/></div> */}
-         
-</Col>
+          <li>
+            <Link to="/myPage/report">
+              <span className='icon'><BsFillMegaphoneFill className='fa' /></span>
+              <span className='title'>REPORT</span>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/myPage/member/MemDetail">
+              <span className='icon'><BsFillInfoCircleFill className='fa' /></span>
+              <span className='title'>회원정보보기</span>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/myPage/member/MemChange">
+              <span className='icon'><BsFillPeopleFill className='fa' /></span>
+              <span className='title'>호스트전환</span>
+            </Link>
+          </li>
+
+        </ul>
+
+        {/* ----------------------------------------------------------------------------------------- */}
+
+        {MEM_LEVEL === 1 && <HostSidebar />}
+        {MEM_LEVEL === 0 && <AdminSidebar />}
+      </div>
+
+      {/* <div class="toggle"><AiOutlineMenu className='fa' onClick={toggleMenu()}/></div> */}
+
+    </Col>
 
   )
 }
