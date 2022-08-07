eef95e77e94f096544fd730ca49d1ff39daadbb0
hoyeon <hoyeon9353@naver.com>
2022-08-08 03:01:03 +0900

    0808

--- front/src/admin/adminQna/AdminInsertQna.js
+++ front/src/admin/adminQna/AdminInsertQna.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
  import {Button,Form} from 'react-bootstrap';
  import { useParams } from 'react-router-dom';
+// import Alert from 'react-bootstrap/Alert';
 
 
-const AdminInsertReport = () => {
+const AdminInsertQna = () => {
 
-    const {REPORT_IDX} = useParams();
+    const {QNA_IDX} = useParams();
+
 
       //-------comment 업데이트
-      const [reportUpdate , setReportUpdate] = useState({
-        comment : '',
+      const [qnaUpdate , setQnaUpdate] = useState({
+        comment : ''
       });
 
       const handleChange = (e) => { //이벤트 e 선언 폼의 text들이 change 될 때마다 e의 값이 넘어감
         const {name,value} = e.target
-        setReportUpdate ({
-          ...reportUpdate, //...기존의 qnaUpdate 값에
+        setQnaUpdate ({
+          ...qnaUpdate, //...기존의 qnaUpdate 값에
         [name]:value
         })
         //console.log(form); //form 얘한테 comment가 담겨있음
@@ -25,19 +27,19 @@ const AdminInsertReport = () => {
       
       //입력 버튼 클릭 시 update 실행해주는 함수 (레벨 미답변->답변완료
      
-      const adminUpdateReport = (e) => { 
+      const adminUpdateQna = (e) => { 
         e.preventDefault();
         axios({
         method : 'post',
-        url : '/GareBnB/Admin/reportComment.do',
+        url : '/GareBnB/Admin/qnaComment.do',
         contentType:"apllication/json; charset=UTF-8",
         params : {
-          REPORT_COMMENT : (reportUpdate.comment),  //입력된 값이 넘어가야함
-          REPORT_IDX : REPORT_IDX
+          QNA_COMMENT : (qnaUpdate.comment),  //입력된 값이 넘어가야함
+          QNA_IDX : QNA_IDX
         }
       }).then(Response => {
         console.log(Response.data);
-        setReportUpdate(Response.data);
+        setQnaUpdate(Response.data);
       });
       window.location.reload();
       }
@@ -45,31 +47,30 @@ const AdminInsertReport = () => {
       const navigate = useNavigate();
 
      return (
-
+    
         <div className='commentF'>
           <div> <h3>답변하기</h3>
-        <Form onSubmit={adminUpdateReport}>
+        <Form onSubmit={adminUpdateQna}>
         <Form.Group controlId="formBasicEmail">
         <Form.Control type="text" placeholder="답변내용을 등록하세요" 
-        onChange={handleChange} name="comment" value={reportUpdate.comment} />
+        onChange={handleChange} name="comment" value={qnaUpdate.comment} />
       </Form.Group>
-
-      <br/>
-      <div className='rbutton text-lg-end'>
+      
+      <div className='qbutton text-lg-end'>
       <Button variant="primary" type="submit">
         입력하기
       </Button>
       </div>
-
+      
       <div className='col-lg-12 text-lg-center'>
         <button type="button" className="btn btn-success"  
         onClick={(e)=>{ e.preventDefault(); navigate(-1); }}>뒤로가기</button>
-      </div>
+      </div> 
 
       </Form>
       </div>
-      </div>  
+      </div>
   );
 };
 
-export default AdminInsertReport;
+export default AdminInsertQna;
