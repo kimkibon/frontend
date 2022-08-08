d2ffdd36ee6becaec86750f889d4e768ee689d60
sa <darkcat90@naver.com>
2022-08-08 23:38:31 +0900

    사진클릭->보드디테일

--- front/src/myPage/reserve/MemUseList.js
+++ front/src/myPage/reserve/MemUseList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import SelectOneFile from '../../commons/Files/SelectOneFile';
+import { Link, } from "react-router-dom";
 
 
 //하나의 이용내역 정보
@@ -23,7 +24,9 @@ const MemUseList = (props) => {
   return (
     <>
       <div className="col-md-3 mt-2 d-flex flex-column align-items-center align-content-center">
+      <Link to='/board/detail' state={{ 'BOARD_NO': list.RES_BOARD_NO,'BOARD_MODIFY_NO':list.RES_BOARD_MODIFY_NO }}>
         <img className="img-fluid img-responsive rounded product-image" src={list.URL} width="200px" height="auto"/><p/>
+      </Link>
       </div>
       <div className="col-md-7 mt-1 align-items-center align-content-center">
                   예약번호 {list.RES_IDX}<br/>
