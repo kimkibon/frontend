ad61eaa060f3115790d12860e9a0561c2044d4a5
aram0111 <aramingk@naver.com>
2022-08-10 13:24:22 +0900

    ff

--- front/src/admin/adminHostMember/AdminHostConfirmList.js
+++ front/src/admin/adminHostMember/AdminHostConfirmList.js
@@ -17,43 +17,44 @@ const AdminHostConfirmList = () => {
       });
   }, []);
 
+  
   return (
-    <div>
-      <div className='container'>
+    hostResList[0] !== undefined ? hostResList.map((list) => {
 
-        <hr />
-        <h3>호스트 등록 요청 리스트</h3>
-        <hr />
+      <div>
+        <div className='container'>
 
-        <Table striped width="900px" height="30px" className="table table-hover">
-          <thead>
-            <tr align='center'>
-              <td width="20%">번호(IDX)</td>
-              <td width="20%">이름</td>
-              <td width="30%">아이디</td>
-              <td width="30%">휴대폰 번호</td>
-            </tr>
-          </thead>
+          <hr />
+          <h3>호스트 등록 요청 리스트</h3>
+          <hr />
 
-          {hostResList[0] !== undefined ? hostResList.map((list) => {
-            return (
-              <tbody key={list.MEM_IDX}>
-                <tr align='center' onClick={() => { window.location.href = '/admin/AdminHostConfirmDetail/' + list.MEM_IDX }}>
-                  <td> {list.MEM_IDX}</td>
-                  <td>{list.MEM_NAME}</td>
-                  <td>{list.MEM_ID}</td>
-                  <td>{list.MEM_PHONE}</td>
-                </tr>
-              </tbody>
-            )
-          })
-            : <div className="mt-5 mb-5 d-flex justify-content-center"><h4>호스트 전환 요청 회원이 없습니다.</h4></div>
-          }
+          <Table striped width="900px" height="30px" className="table table-hover">
+            <thead>
+              <tr align='center'>
+                <td width="20%">번호(IDX)</td>
+                <td width="20%">이름</td>
+                <td width="30%">아이디</td>
+                <td width="30%">휴대폰 번호</td>
+              </tr>
+            </thead>
 
-        </Table>
+            <tbody key={list.MEM_IDX}>
+              <tr align='center' onClick={() => { window.location.href = '/admin/AdminHostConfirmDetail/' + list.MEM_IDX }}>
+                <td> {list.MEM_IDX}</td>
+                <td>{list.MEM_NAME}</td>
+                <td>{list.MEM_ID}</td>
+                <td>{list.MEM_PHONE}</td>
+              </tr>
+            </tbody>
+           
+          </Table>
+        </div>
       </div>
-    </div>
+})
+: <div className="mt-5 mb-5 d-flex justify-content-center "><h4>호스트 전환 요청 회원이 없습니다.</h4></div>
   )
-}
+    }
 
-export default AdminHostConfirmList
+  export default AdminHostConfirmList
+
+// {/* : <div className="mt-5 mb-5 justify-content-center align-items-center"><h4>호스트 전환 요청 회원이 없습니다.</h4></div> */}
